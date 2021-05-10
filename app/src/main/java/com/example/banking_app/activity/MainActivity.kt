ebca676749aa1ad29c47cc2061a639f09eab7e82
Sociu Daniel <danielsociu00@gmail.com>
2021-05-10 13:51:09 +0300

    Added bottom-nav bar

--- app/src/main/java/com/example/banking_app/activity/MainActivity.kt
+++ app/src/main/java/com/example/banking_app/activity/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.banking_app.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.banking_app.R
 import com.example.banking_app.fragments.OverviewFragment
+import com.example.banking_app.fragments.PaymentsFragment
+import com.example.banking_app.fragments.ProfileFragment
 import com.example.banking_app.ui.main.SectionsPagerAdapter
-import com.google.android.material.navigation.NavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigationView);
+        navigationView.setOnNavigationItemSelectedListener(this);
 
         //val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
 
@@ -34,5 +43,26 @@ class MainActivity : AppCompatActivity() {
         if(savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(R.id.container, OverviewFragment()).commit()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_overview -> {
+                val fragment = OverviewFragment();
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
+            }
+            R.id.navigation_payments -> {
+                val fragment = PaymentsFragment();
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
+            }
+            R.id.navigation_profile -> {
+                val fragment = ProfileFragment();
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/banking_app/fragments/ProfileFragment.kt
@@ -0,0 +1,17 @@
+package com.example.banking_app.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.banking_app.R
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_profile, container, false);
+        activity?.title = getString(R.string.nav_profile);
+        return view;
+    }
+}
