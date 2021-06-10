a24b3806791c7ecc79fad9a4104a120791c6e7e1
Kira060200 <constantin.gab02@gmail.com>
2021-06-10 18:38:52 +0300

    implementing card listing

--- app/src/main/java/com/example/banking_app/config/DatabaseConnection.java
+++ app/src/main/java/com/example/banking_app/config/DatabaseConnection.java
@@ -1,5 +1,7 @@
 package com.example.banking_app.config;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
@@ -7,15 +9,51 @@ import java.util.Properties;
 public class DatabaseConnection {
     private static Connection connection;
 
-    private DatabaseConnection() {}
+    public DatabaseConnection() {}
 
-    public static Connection getConnection() throws SQLException {
+    public ResultSet getCards() throws SQLException{
+        //Connection con = getConnection();
+        Properties databaseProp = new Properties();
+        try {
+            databaseProp.load(getClass().getClassLoader().getResourceAsStream("JDBCcredentials.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            //lookup the mysql module
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.d("ClassTag", "Failed1");
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://" + databaseProp.getProperty("databaseIP") + ":" + databaseProp.getProperty("databasePort") +
+                    "/" + databaseProp.getProperty("databaseName") + "?user=" + databaseProp.getProperty("databaseUsername") + "&password=" + databaseProp.getProperty("databasePassword"));
+            Statement stmt = con.createStatement();
+            return stmt.executeQuery(
+                    "select * from CARD;"
+            );
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Log.d("SQLTag", "Failed to execute");
+        }
+        return null;
+    }
+
+    public Connection getConnection() throws SQLException {
         if (connection == null) {
             Properties databaseProp = new Properties();
             try {
                 databaseProp.load(DatabaseConnection.class.getClassLoader().getResourceAsStream("JDBCcredentials.properties"));
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+            try {
+                //lookup the mysql module
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                Log.d("ClassTag", "Failed1");
             }
             connection = DriverManager.getConnection("jdbc:mysql://" + databaseProp.getProperty("databaseIP") + ":" + databaseProp.getProperty("databasePort") +
                     "/" + databaseProp.getProperty("databaseName") + "?user=" + databaseProp.getProperty("databaseUsername") + "&password=" + databaseProp.getProperty("databasePassword"));
--- app/src/main/java/com/example/banking_app/fragments/CardsFragment.kt
+++ app/src/main/java/com/example/banking_app/fragments/CardsFragment.kt
@@ -1,14 +1,24 @@
 package com.example.banking_app.fragments
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.banking_app.R
 import com.example.banking_app.activity.GenerateCardActivity
+import com.mysql.jdbc.Connection
+import java.io.IOException
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.SQLException
+import java.util.*
+
 
 class CardsFragment: Fragment() {
     override fun onCreateView(
@@ -24,7 +34,105 @@ class CardsFragment: Fragment() {
             intent.putExtra("extra_iban", iban)
             startActivity(intent)
         }
+        val cardsLayout : LinearLayout = view.findViewById(R.id.overviewHolder);
+
+        addCards(inflater, cardsLayout)
+
         return view;
+    }
+
+
+    @SuppressLint("SetTextI18n", "ResourceType")
+    private fun addCards(
+        inflater: LayoutInflater,
+        location: LinearLayout
+    ) {
+
+        val databaseProp = Properties()
+        try {
+            databaseProp.load(javaClass.classLoader.getResourceAsStream("JDBCcredentials.properties"))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        try {
+            //lookup the mysql module
+            Class.forName("com.mysql.jdbc.Driver").newInstance()
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+            Log.d("ClassTag", "Failed1")
+        }
+        try {
+            Log.d("ClassTag", databaseProp.getProperty("databaseIP"))
+            Log.d("ClassTag", databaseProp.getProperty("databaseName"))
+            Log.d("ClassTag", databaseProp.getProperty("databasePassword"))
+            val con: Connection = DriverManager.getConnection(
+                "jdbc:mysql://" + databaseProp.getProperty("databaseIP") + ":" + databaseProp.getProperty(
+                    "databasePort"
+                ) +
+                        "/" + databaseProp.getProperty("databaseName") + "?user=" + databaseProp.getProperty(
+                    "databaseUsername"
+                ) + "&password=" + databaseProp.getProperty("databasePassword")
+            ) as Connection
+            Log.d("ClassTag", databaseProp.getProperty("databaseIP"))
+            Log.d("ClassTag", databaseProp.getProperty("databaseName"))
+            Log.d("ClassTag", databaseProp.getProperty("databasePassword"))
+
+            val stmt = con.createStatement()
+            val result: ResultSet = stmt.executeQuery(
+                "select * from CARD;"
+            )
+            while(result.next()){
+                val hold: View = inflater.inflate(R.layout.log_cards, location, false);
+                val id = result.getString(1)
+                val cvv2 = result.getString(3)
+                val type = result.getString(4)
+                val validity = result.getString(5)
+
+
+                val idText: TextView = hold.findViewById(R.id.cardLogId)
+                idText.text = id
+                val cvv2Text: TextView = hold.findViewById(R.id.cardLogCVV2)
+                cvv2Text.text = cvv2
+                val typeText: TextView = hold.findViewById(R.id.cardLogType)
+                typeText.text = type
+                val validityText: TextView = hold.findViewById(R.id.cardLogValidity)
+                validityText.text = validity
+
+                hold.setOnClickListener {
+                    //val accountDetailsIntent = Intent(context, AccountDetailsActivity::class.java);
+                    val popupMenu: PopupMenu = PopupMenu(activity, hold)
+                    popupMenu.menuInflater.inflate(R.layout.card_popup_menu, popupMenu.menu)
+                    popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                        when (item.itemId) {
+                            R.id.renew_card ->
+                                Toast.makeText(
+                                    activity,
+                                    "You Clicked : " + item.title,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            R.id.close_card ->
+                                Toast.makeText(
+                                    activity,
+                                    "You Clicked : " + item.title,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                        }
+                        true
+                    })
+                    popupMenu.show()
+                //val bundle = Bundle();
+                    //bundle.putString("iban", account.getIban());
+                    //accountDetailsIntent.putExtras(bundle);
+
+                    //startActivity(accountDetailsIntent);
+                }
+
+                location.addView(hold);
+            }
+        } catch (ex: SQLException) {
+            ex.printStackTrace()
+            Log.d("SQLTag", "Failed to execute")
+        }
     }
 
 }
